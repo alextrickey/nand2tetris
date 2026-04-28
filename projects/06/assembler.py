3530dfcb82fe4effd5b80407278cceca6238dec5
Alex Trickey <k.alex.trickey@gmail.com>
2026-04-28 10:26:45 +0100

    Parse jump and destination commands

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -15,7 +15,7 @@ LABEL = r"[a-zA-Z]+\w*"  # Starts with a letter, contains only letters, digits, 
 LABEL_CMD = r"^\(" + LABEL + r"\)$"
 
 # Compute Command Regex ("Destination=Result;Jump")
-DESTINATION = r"([AMD]+=)?"
+DESTINATION = r"^([AMD]+=)"
 
 RESULT_0 = r"0"
 RESULT_1 = r"-?1"
@@ -32,11 +32,18 @@ RESULT = (
     r"))"
 )
 
-JUMP_0 = r";JMP"
-JUMP_1 = r";J[GL][TE]"
-JUMP = r"((" + JUMP_0 + r")|(" + JUMP_1 + r"))?"
+JUMP_0 = r";JMP$"
+JUMP_1 = r";J[GL][TE]$"
+JUMP_2 = r";JEQ$"
+JUMP_3 = r";JNE$"
+JUMP = (r"((" + 
+    JUMP_0 + r")|(" + 
+    JUMP_1 + r")|(" +
+    JUMP_2 + r")|(" +
+    JUMP_3 + r"))"
+)
 
-COMPUTE_CMD = r"^" + DESTINATION + RESULT + JUMP + r"$"
+COMPUTE_CMD = DESTINATION + r"?" + RESULT + JUMP + r"?"
 
 
 # Jump Patterns
@@ -51,7 +58,6 @@ class Parser:
         
         match = re.search(ADDRESS_CMD, command)
         if match:
-	        # ADDRESS = self.command[1:match.span()[1]]
             return 'A_COMMAND'
         
         match = re.search(LABEL_CMD, command)
@@ -63,7 +69,6 @@ class Parser:
             return 'C_COMMAND'
         
         raise SyntaxError(f"Unrecognized command format: {command}")
-
 
     def find_commands(self):
         # TODO Add label command filter/processing here:
@@ -84,6 +89,34 @@ class Parser:
     def _is_empty_string(self, line):
         return line == ""
 
+    def parse_label(self, l_command):
+        match = re.search(LABEL, l_command)
+        return l_command[match.span()]
+
+    def get_symbol(self, a_command):
+        pass
+
+    def get_destinations(self, c_command):
+        match = re.search(DESTINATION, c_command)
+        if match: 
+            dest = c_command[match.start():match.end()]
+            return [d for d in ['A', 'M', 'D'] if d in dest]
+        else: 
+            return None
+
+    def get_jump(self, c_command):
+        match = re.search(JUMP, c_command)
+        if match:
+            return c_command[match.start()+1:]
+        else: 
+            return None
+
+    def get_compute(self, c_command):
+        c_command = re.sub(DESTINATION, "", c_command)
+        c_command = re.sub(JUMP, "", c_command)
+        # TODO
+
+
 class Code:
     pass
 
@@ -102,4 +135,12 @@ if __name__ == "__main__":
     p.find_commands()
 
     for c in p.commands:
-        print(c, p.command_type(c))
+        ct = p.command_type(c)
+        if ct == 'C_COMMAND':
+            d = p.get_destinations(c)
+            j = p.get_jump(c)
+        else: 
+            d=None
+            j=None
+        print(c, ct, d, j)
+        
