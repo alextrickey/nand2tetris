78e7f41cca16563a791f880a8a7f4d637113e495
Alex Trickey <k.alex.trickey@gmail.com>
2026-05-07 16:55:05 +0100

    Document the assembler commands

--- projects/assembler/constants.py
+++ projects/assembler/constants.py
@@ -1,10 +1,18 @@
+#########################################
+# Computation Nmemonic to Code Mappings #
+#########################################
 
+# Computation Command Format Fed to ALU:  111a cccc ccdd djjj
+# where the a, c's, d's and j's are defined below.
 
-##########################
-# Code Nmemonic Mappings #
-##########################
+# Destinations
+# Code format = d1 d2 d3
+# - if d1=1 then store result in Register A
+# - if d2=1 then store result in Register D
+# - if d3=1 then store result in Memory location addressed in A
 
 DEST_MNEMONICS = {
+    # Assign to all three locations
     'AMD': '111',
     'ADM': '111',
     'MDA': '111',
@@ -12,6 +20,7 @@ DEST_MNEMONICS = {
     'DMA': '111',
     'DAM': '111',
 
+    # Assign to two locations
     'AM': '101',
     'MA': '101',
     'AD': '110',
@@ -19,56 +28,76 @@ DEST_MNEMONICS = {
     'MD': '011',
     'DM': '011',
 
+    # Assign to one location
     'A': '100',
     'M': '001',
     'D': '010',
 
+    # No assignment
     None: '000'
 }
 
+# Jump Conditions
+# Code format = j1 j2 j3
+# - if j1=1 then jump if result is less than 0
+# - if j2=1 then jump if result equals 0
+# - if j3=1 then jump if result is greater than 0
 
-# Format: 'LEG'
 JUMP_MNEMONICS = {
-    None:  '000',
-    'JGT': '001',
-    'JEQ': '010',
-    'JGE': '011',
-    'JLT': '100',
-    'JNE': '101',
-    'JLE': '110',
-    'JMP': '111',
+    None:  '000',  # Don't Jump
+    'JGT': '001',  # Jump if Greater Than 0
+    'JEQ': '010',  # Jump if Equal to 0
+    'JGE': '011',  # Jump if Greater than or Equal to 0
+    'JLT': '100',  # Jump if Less than 0
+    'JNE': '101',  # Jump if Not Equal to 0
+    'JLE': '110',  # Jump if Less than or Equal to 0
+    'JMP': '111',  # Jump unconditionally
 }
 
-# Format:   'acccccc'
+# ALU Operations 
+# Code format = a c1 c2 c3 c4 c5 c6
+# - if a=0 then A is used for the computation, otherwise M is used
+# 
+# The c codes are inputs to the ALU 
+# and have the following meanings: 
+# - if c1=1 the first ALU input (D) is set to 0
+# - if c2=1 the first ALU input (D) is negated
+# - if c3=1 the second ALU input (A or M) is set to 0
+# - if c4=1 the second ALU input (A or M) is negated
+# - if c5=1 the computation is arithmetic (+) otherwise logical (&)
+# - if c6=1 the computation result is negated
+#
+# For further detail, reference the ALU component HDL and 
+# the Nand2Tetris documentation. 
 COMP_MNEMONICS = {
-    '0':    '0101010',
-    '1':    '0111111',
-    '-1':   '0111010',
-    'D':    '0001100',
-    'A':    '0110000',
-    'M':    '1110000',
-    '!D':   '0001101',
-    '!A':   '0110001',
-    '!M':   '1110001',
-    '-D':   '0001111',
-    '-A':   '0110011',
-    '-M':   '1110011',
-    'D+1':  '0011111',
-    'A+1':  '0110111',
-    'M+1':  '1110111',
-    'D-1':  '0001110',
-    'A-1':  '0110010',
-    'M-1':  '1110010',
-    'D+A':  '0000010',
-    'D+M':  '1000010',
-    'D-A':  '0010011',
-    'D-M':  '1010011',
-    'A-D':  '0000111',
-    'M-D':  '1000111',
-    'D&A':  '0000000',
-    'D&M':  '1000000',
-    'D|A':  '0010101',
-    'D|M':  '1010101',
+    '0':    '0' + '101010',
+    '1':    '0' + '111111',
+    '-1':   '0' + '111010',
+    'D':    '0' + '001100',
+    'A':    '0' + '110000',
+    'M':    '1' + '110000',
+    '!D':   '0' + '001101',
+    '!A':   '0' + '110001',
+    '!M':   '1' + '110001',
+    '-D':   '0' + '001111',
+    '-A':   '0' + '110011',
+    '-M':   '1' + '110011',
+    'D+1':  '0' + '011111',
+    'A+1':  '0' + '110111',
+    'M+1':  '1' + '110111',
+    'D-1':  '0' + '001110',
+    'A-1':  '0' + '110010',
+    'M-1':  '1' + '110010',
+    'D+A':  '0' + '000010',
+    'D+M':  '1' + '000010',
+    'D-A':  '0' + '010011',
+    'D-M':  '1' + '010011',
+    'A-D':  '0' + '000111',
+    'M-D':  '1' + '000111',
+    'D&A':  '0' + '000000',
+    'D&M':  '1' + '000000',
+    'D|A':  '0' + '010101',
+    'D|M':  '1' + '010101',
 }
 
 ######################
---
+++ projects/assembler/readme.md
@@ -0,0 +1,82 @@
+# Assembler
+
+## Overview
+This directory contains a python module to parse an assembly script and
+assemble the relevant binary codes to create a ROM script that can be run 
+on the HACK hardware defined under `../hardware_definition`.
+
+Example assembly files (`.asm`) and binary files (`.hack`) can be found
+in the `examples` directory and detailed specifications for each can be 
+found in the [Nand2Tetris resource](https://www.nand2tetris.org). 
+
+In brief, the assembler supports three common assembly command types: 
+### 1. Labeling
+Syntax: `(LABEL)` where `LABEL` is a variable name/symbol which will 
+indicate the ROM address of the next relevant command. 
+
+Labels only create symbols that can be referenced lat addressing and do 
+not themselves result in any binary command. 
+
+### 2. Addressing
+Syntax: `@address` where `address` can be an integer indicating a 
+RAM location or a compliant variable/label name. A variable name can  
+reference existing symbol with an address defined elsewhere the assembly 
+file or the standard set (defined in `constants.RAM_SYMBOLS`). Alternatively, 
+it can define a new variable at the next available RAM address. 
+
+The generated machine code has format: ```0vvv vvvv vvvv vvvv``` where the 
+leading zero indicates an address is being provided and the remaing `v`s 
+can be `0` or `1`, such that they provide an integer address. 
+
+
+### 3. Computations
+Syntax: `dest=comp;jump` where 
+    - `dest` indicates the destination where the result of the computation 
+    should be stored. It can be any combination of A, D, M where A and D
+    are registers and M is the memory at the address in the A register. 
+    This component is optional and if no destination is provided the 
+    computation result will not be stored. 
+    - `comp` indicates the computation to be completed, which can include
+    simple arithmetic computations (e.g. `A+D`, `D-M`, and `-A`) and 
+    bitwise logical operations (e.g. `A&D`, `A|D`, `!A`). This component 
+    is required. 
+    - `jump` indicates what computation results should trigger a jump 
+    to the ROM line stored in the A register. An unconditional jump can
+    be specified with `JMP`, while other codes will compare computation 
+    result to zero jump if it greater than/less than or equal to zero 
+    (e.g. `JGT`, `JLT`, `JEQ`, `JLE`)
+
+The generated machine code has format: ```111a cccc ccdd djjj``` where the 
+3 leading ones indicate the a, c, d, and j bits define a computation. 
+Detailed descriptions of the mnemonic to code mappings can be viewed 
+in `assembler/constants.py`.
+
+
+## Setup 
+Requires Python 3.9+ and uses only standard libraries. 
+
+## Running the Assembler
+To build binaries for an assembly (`.asm`) file navigate to the assembler
+directory and then call the function on desired file. For example, 
+to build the binaries for the file 'example/Add.asm': 
+
+```bash
+cd path/to/assembler
+./assembler.py example/Add.asm
+```
+
+The above will build the binary file `Add.hack` in the directory in the 
+`example` directory. 
+
+To build both the binary and write an additional debugging file containing 
+further information about each command, use the `--debug` flag. 
+```bash
+cd path/to/assembler
+./assembler.py example/Add.asm --debug 
+```
+
+## Running Tests
+From the shell, run:  
+```bash
+python3 -m unittest discover tests/
+```
