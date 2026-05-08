1c75faf613664628a269a271742f31ebc7d66c21
Alex Trickey <k.alex.trickey@gmail.com>
2026-05-08 11:37:42 +0100

    Test utils

--- projects/assembler/tests/test_utils.py
+++ projects/assembler/tests/test_utils.py
@@ -1,11 +1,45 @@
 import unittest
-import utils
+from assembler import utils
 
 
 class TestUtils(unittest.TestCase):
+    
+    def test_remove_whitespace(self):
+
+        input = 'no   s pa \n\t ce'
+        actual = utils.remove_whitespace(line=input)
+        expect = 'nospace'
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
+    
+    def test_is_empty_string(self):
+
+        actual = utils.is_empty_string(line='')
+        expect = True
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
+
+        actual = utils.is_empty_string(line=' ')
+        expect = False
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
+    
+    def test_regex_any(self):
+
+        input = ['a', 'b', 'c']
+        actual = utils.regex_any(input)
+        expect = '((a)|(b)|(c))'
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
+
+        input = ['a']
+        actual = utils.regex_any(input)
+        expect = '((a))'
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
+
+        input = []
+        actual = utils.regex_any(input)
+        expect = '(())'
+        self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
 
     def test_binary_string_from_int(self):
-
+        
         actual = utils.binary_string_from_int(value=3)
         expect = '11'
         self.assertEqual(actual, expect, "Observed: {actual}, Expected: {expect}")
